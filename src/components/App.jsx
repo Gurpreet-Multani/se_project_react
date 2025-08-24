9a83047d67dde1d17a6efc9d21a3cc4ffee6022c
Gurpreet-Multani <gmultani133@gmail.com>
2025-08-23 20:09:50 -0700

    Bug/Fixed Header import

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,11 @@ import { useState } from "react";
 import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
 import "../blocks/App.css";
-import Header from "./Header";
+import "./Header";
 import "./Main";
 import "./Footer";
 import "./ModalWithForm";
 import "./ItemModal";
-
-//header component
-//logo aka WTWR img
-//The current date that can be generated using the Date() object’s toLocaleString() method.
-//An “Add Clothes” button that opens ModalWithForm
-//The user’s name, location, and avatar (all hard-coded at this point)
 
 function App() {}
 
