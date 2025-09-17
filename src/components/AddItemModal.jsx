9d46f27c952987f45683d7ba6868846ae293c7b4
Gurpreet-Multani <gmultani133@gmail.com>
2025-09-16 21:27:49 -0700

    Updated/Project 11

--- src/components/AddItemModal.jsx
+++ src/components/AddItemModal.jsx
@@ -1,7 +1,10 @@
-//ignore this file
-
 function ItemModal() {
-    return console.log(IGNORE THIS)
+  // It's not common to log something on every render, but this is syntactically correct
+  console.log("IGNORE THIS");
+  return (
+    // You must return JSX or null from a React component
+    <div>This is the ItemModal.</div>
+  );
 }
 
-export ItemModal
+export default ItemModal;
